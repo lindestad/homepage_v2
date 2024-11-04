24586feeaf537593330c4debc8045ec32e25f238
Daniel Lindestad <daniel17@uia.no>
2024-11-04 14:56:47 +0100

    Version one finalized

---
+++ links.js
@@ -0,0 +1,13 @@
+// Add additional links in this section. The icon should be placed in the icons folder.
+const iconData = [
+    {
+        "icon": "icons/book.svg",
+        "url": "https://en.wikipedia.org/wiki/Main_Page",
+        "label": "Wikipedia"
+    },
+    {
+        "icon": "icons/code.svg",
+        "url": "https://heroicons.com/",
+        "label": "Add more links in links.js"
+    }
+];
--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-function navigate(url) {
-    window.location.href = url;
+function loadIcons() {
+    const grid = document.getElementById('icon-grid');
+    
+    iconData.forEach(item => {
+        const card = document.createElement('div');
+        card.classList.add('icon-card');
+        card.onclick = () => window.location.href = item.url;
+        
+        const icon = document.createElement('img');
+        icon.src = item.icon;
+        icon.alt = item.label;
+        card.appendChild(icon);
+        
+        const label = document.createElement('p');
+        label.textContent = item.label;
+        card.appendChild(label);
+        
+        grid.appendChild(card);
+    });
 }
+
+loadIcons();
